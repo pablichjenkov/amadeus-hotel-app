930b553a301b170a3301ff173d3201c6436dbcc8
Pablo Valdes <am.pablo.vc@gmail.com>
2023-09-28 20:27:20 -0400

    Bump component-toolkit to 0.5.7

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -43,7 +43,7 @@ kotlin {
                 implementation(compose.foundation)
                 implementation(compose.ui)
                 implementation(compose.material3)
-                api("io.github.pablichjenkov:component-toolkit:0.5.6")
+                api("io.github.pablichjenkov:component-toolkit:0.5.7")
                 implementation("io.github.pablichjenkov:amadeus-api:0.3.2")
             }
         }
